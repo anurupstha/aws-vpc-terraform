e5fa0e54d6a61fa260ed9d1f987c42cf54ea22d3
anurupstha <53064464+anurupstha@users.noreply.github.com>
2024-02-05 21:59:13 -0500

    Commit-04

--- main.tf
+++ main.tf
@@ -23,4 +23,25 @@ terraform {
   } 
 }
 
+# Create Public Subnet
+resource "aws_subnet" "public-subnet" {
+  count = length(var.public_subnet_cidr)
+  vpc_id     = aws_vpc.vpc-demo-us-east-1.id
+  cidr_block = var.public_subnet_cidr[count.index]
+  availability_zone = var.availability_zone[count.index]
+  tags = {
+    Name = "Subnet-Public: Public Subnet-${count.index + 1}"
+  }
+}
+
+# Create private subnet 
+resource "aws_subnet" "private-subnet" {
+  count = length(var.private_subnet_cidr)
+  vpc_id     = aws_vpc.vpc-demo-us-east-1.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = var.availability_zone[count.index]
+  tags = {
+    Name = "Subnet-Private: Private Subnet-${count.index + 1}"
+  }
+}
    
