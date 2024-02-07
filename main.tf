2a8b486a06973ea84c50517b5e9c5e20d984214b
anurupstha <53064464+anurupstha@users.noreply.github.com>
2024-02-06 21:56:04 -0500

    Route Table Commit

--- main.tf
+++ main.tf
@@ -67,3 +67,31 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "Private NAT GW: For aws-vpc-project"
   }
 }
+
+# Create Route Table for Public Subnet
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc-demo-us-east-1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public_internet_gateway.id
+  }
+    tags = {
+        Name = "RT Public Route Table: For aws-vpc-project"
+    }
+}    
+
+# Create Route table for private subnet
+resource "aws_route_table" "private_route_table" {
+  count = length(var.private_subnet_cidr)
+  vpc_id = aws_vpc.vpc-demo-us-east-1.id
+  depends_on = [ aws_nat_gateway.nat_gateway ]
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
+    }
+    tags = {
+      Name = "RT Private Route Table: For aws-vpc-project"
+    }
+}
+
+ 
